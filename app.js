a1ec6f18d9cb47186bbf17f6fd20f649b0a51ee0
xrchitron <yuahochen1124@gmail.com>
2023-08-21 11:19:13 +0800

    feat: add listing page

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const { engine } = require("express-handlebars");
 const app = express();
 const port = 3000;
+const movies = require("./public/jsons/movies.json").results;
+const BASE_IMG_URL = "https://movie-list.alphacamp.io/posters/";
 
 app.engine(".hbs", engine({ extname: ".hbs" }));
 app.set("view engine", ".hbs");
@@ -13,7 +15,7 @@ app.get("/", (req, res) => {
 });
 
 app.get("/movies", (req, res) => {
-  res.render("index");
+  res.render("index", { movies, BASE_IMG_URL });
 });
 
 // app.get("/movies", (req, res) => {
