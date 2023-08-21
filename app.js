5945b69576a53fa261f4e341da269897713717ed
xrchitron <yuahochen1124@gmail.com>
2023-08-21 15:02:43 +0800

    feat: add search function to listing

--- app.js
+++ app.js
@@ -15,12 +15,25 @@ app.get("/", (req, res) => {
 });
 
 app.get("/movies", (req, res) => {
-  res.render("index", { movies, BASE_IMG_URL });
+  //the ? operator allows JavaScript to attempt to call the trim() function only if req.query.search is not null or undefined
+  // If req.query.search is null or undefined, it will not throw an error, instead, the expression will return undefined
+  //let keyword = (req.query.search !== null && req.query.search !== undefined) ? req.query.search.trim() : undefined; (traditional method)
+  const keyword = req.query.search?.trim(); //new method from ES11
+  const matchedMovies = keyword
+    ? movies.filter((mv) =>
+        Object.values(mv).some((property) => {
+          if (typeof property === "string") {
+            return property.toLowerCase().includes(keyword.toLowerCase());
+          }
+          return false;
+        })
+      )
+    : movies;
+  res.render("index", { movies: matchedMovies, BASE_IMG_URL, keyword });
 });
-
 // app.get("/movies", (req, res) => {
 //   res.render("index", { layout: "layoutTry" });
-// });
+// }); //method to apply other template
 
 app.get("/movies/:id", (req, res) => {
   const id = req.params.id;
